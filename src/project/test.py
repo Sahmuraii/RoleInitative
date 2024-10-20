1001268dce4f48e3eefcd4adfb7465e997898ec5
Yensid626 <43460104+Yensid626@users.noreply.github.com>
2024-10-20 16:21:38 -0700

    Improved security
    Removed passwords from code and added them to .env file, and added password hashing to prevent storing of raw passwords.

--- src/project/test.py
+++ src/project/test.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user
+from dotenv import load_dotenv
+from os import getenv
+import bcrypt
+
+# Load environment variables from the .env file (if present)
+load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://{user}:{pw}@{url}/{db}'.format(user="dungeon_master",pw="Roll4Initiative#Nat20_Crit",url="71.93.81.236:10020",db="RoleInitiative")
-app.config["SECRET_KEY"] = "abc"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=getenv('DATABASE_USER'),pw=getenv('DATABASE_PASSWORD'),url=getenv('DATABASE_URL'),db=getenv('DATABASE_NAME'))
+app.config["SECRET_KEY"] = getenv('FLASK_SECRET_KEY')
 db = SQLAlchemy()
 
 login_manager = LoginManager()
@@ -31,10 +37,16 @@ def loader_user(user_id):
 @app.route('/register', methods=["GET", "POST"])
 def register():
 	if request.method == "POST":
-		user = Users(username=request.form.get("username"),
-					password=request.form.get("password"))
+		requested_username = request.form.get("username")
+		requested_pasword = request.form.get("password")
+
+		password_hash = bcrypt.hashpw(requested_pasword.encode('utf-8'), bcrypt.gensalt()).decode('utf-8') #obscure raw password
+
+		user = Users(username=requested_username,
+					password=password_hash)
 		db.session.add(user)
 		db.session.commit()
+
 		return redirect(url_for("login"))
 	return render_template("sign_up.html")
 
@@ -44,7 +56,13 @@ def login():
 	if request.method == "POST":
 		user = Users.query.filter_by(
 			username=request.form.get("username")).first()
-		if user.password == request.form.get("password"):
+	
+		if user is None:
+			return render_template("login.html")
+		
+		entered_password = request.form.get("password")
+		password_match = bcrypt.checkpw(entered_password.encode('utf-8'), user.password.encode('utf-8'))
+		if password_match:
 			login_user(user)
 			return redirect(url_for("home"))
 	return render_template("login.html")
