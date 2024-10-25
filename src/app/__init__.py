4812497c3d7bc3c9337df6676a03d6b3187a8d8c
Sahmuraii <vericwilliam@gmail.com>
2024-10-24 20:05:16 -0700

    Update __init__.py

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,45 +2,46 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from os import getenv
+from . import models
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
+
+# Load configuration from config.py or environment variables
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://{user}:{pw}@{url}/{db}'.format(
+    user=getenv('DATABASE_USER'),
+    pw=getenv('DATABASE_PASSWORD'),
+    url=getenv('DATABASE_URL'),
+    db=getenv('DATABASE_NAME')
+)
+app.config["SECRET_KEY"] = getenv('FLASK_SECRET_KEY')
     
-    # Load configuration from config.py or environment variables
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://{user}:{pw}@{url}/{db}'.format(
-        user=getenv('DATABASE_USER'),
-        pw=getenv('DATABASE_PASSWORD'),
-        url=getenv('DATABASE_URL'),
-        db=getenv('DATABASE_NAME')
-    )
-    app.config["SECRET_KEY"] = getenv('FLASK_SECRET_KEY')
-    
-    # Initialize database and login manager
-    db.init_app(app)
-    login_manager.init_app(app)
 
-    # Import Blueprints
-    from app.auth import auth_bp
-    from app.profile import profile_bp
+db.init_app(app)
+login_manager.init_app(app)
 
-    # Register Blueprints
-    app.register_blueprint(auth_bp, url_prefix='/auth')
-    app.register_blueprint(profile_bp, url_prefix='/profile')
+with app.app_context():
+    db.create_all()
 
-    # Create database tables if they don't exist
-    with app.app_context():
-        db.create_all()
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return Users.query.get(user_id)
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-    # Home route
-    @app.route('/')
-    def home():
-        return render_template('home.html')
+from app.auth import auth_bp
+from app.profile import profile_bp
 
-    return app
+# Register Blueprints
+app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(profile_bp, url_prefix='/profile')
+
+# Home route
+@app.route('/')
+def home():
+    return render_template('home.html')
+
