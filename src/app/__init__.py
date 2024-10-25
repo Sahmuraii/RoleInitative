bd3a32d003a549d1173aa1c4a945190ca2efb79d
Sahmuraii <vericwilliam@gmail.com>
2024-10-24 20:30:51 -0700

    Update __init__.py

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from os import getenv
-from models import db
+from app.models import db
 
 app = Flask(__name__)
 
