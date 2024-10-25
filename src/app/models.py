525380e50206fe1f1deb9ba28db9a108827f776a
Sahmuraii <vericwilliam@gmail.com>
2024-10-24 20:18:50 -0700

    Update models.py

--- src/app/models.py
+++ src/app/models.py
@@ -2,8 +2,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
-db = SQLAlchemy()
-
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250), unique=True, nullable=False)
