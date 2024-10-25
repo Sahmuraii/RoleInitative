9731a0aac732ff955403cfce6eafc3c4f51ea37d
Sahmuraii <vericwilliam@gmail.com>
2024-10-24 20:09:25 -0700

    Update __init__.py

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from os import getenv
-from . import models
-
+from app.models import Users
 db = SQLAlchemy()
 login_manager = LoginManager()
 
