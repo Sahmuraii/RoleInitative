b2064dbfd57fe150f96a9be8145edcb474160a91
Brendan Ingersoll <brendaningersoll136@gmail.com>
2024-10-27 16:20:12 -0700

    Added Characters Table and updated Profiles to display characters

--- src/app/models.py
+++ src/app/models.py
@@ -8,3 +8,14 @@ class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
+    chars = db.relationship("Characters", backref="owner", lazy="dynamic")
+
+class Characters(db.Model):
+    char_id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    campaign_id = db.Column(db.Integer)
+    name = db.Column(db.String(250), nullable = False)
+    class_id = db.Column(db.ARRAY(db.Integer), nullable = False)
+    subclass_id = db.Column(db.ARRAY(db.Integer))
+    level = db.Column(db.ARRAY(db.Integer), nullable = False)
+    race_id = db.Column(db.Integer, nullable = False)
