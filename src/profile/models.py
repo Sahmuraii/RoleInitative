5fbf659666304a125a30ed27260307cb00f4ca3e
Sahmuraii <vericwilliam@gmail.com>
2024-11-11 18:42:09 -0800

    models uploaded

--- src/profile/models.py
+++ src/profile/models.py
@@ -5,12 +5,16 @@ class Character(db.Model):
     __tablename__ = "character"
     char_id = db.Column(db.Integer, primary_key=True, nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    class_id = db.Column(db.Integer, nullable=False)
+    subclass_id = db.Column(db.Integer, nullable=False)
     campaign_id = db.Column(db.Integer, nullable=True)
     name = db.Column(db.String(250), nullable=False)
     alignment = db.Column(db.String(50), nullable=True)
     faith = db.Column(db.String(50), nullable=True)
     proficency_bonus = db.Column(db.Integer, nullable=True)
     total_level = db.Column(db.Integer, nullable=True)
+    level = db.Column(db.Integer, nullable=True)
+    race_id = db.Column(db.Integer, nullable=False)
 
 class DNDRace(db.Model):
     __tablename__ = "dnd_race"
@@ -24,7 +28,6 @@ class DNDRace(db.Model):
 
 class Character_Race(db.Model):
     __tablename__ = "character_race"
-    char_id = db.Column(db.Integer, db.ForeignKey('character.char_id'), primary_key=True, nullable=False)
     char_id = db.Column(db.Integer, db.ForeignKey(Character.char_id), primary_key=True, nullable=False)
     is_offical = db.Column(db.Boolean, nullable=True) 
     race_id = db.Column(db.Integer, db.ForeignKey(DNDRace.race_id), nullable=False)
@@ -33,12 +36,6 @@ class Character_Race(db.Model):
 #    __tablename__ = "race_proficiency_option"
 #    proficiency_list_id = db.Column(db.Integer, db.ForeignKey('Proficiency_List.proficiency_list_id'), primary_key=True, nullable=False)
 #    given_by_race = db.Column(db.Integer, db.ForeignKey(''), nullable=False)
-
-class Character_Class(db.Model):
-    __tablename__ = "character_class"
-    char_id = db.Column(db.Integer, db.ForeignKey('character.char_id'), primary_key=True, nullable=False)
-    class_id = db.Column(db.Integer, db.ForeignKey('DND.class_id'), nullable=False)
-    class_level = db.Column(db.Integer, nullable=False)
 
 class DND_Class(db.Model):
     __tablename__ = "dnd_class"
@@ -49,6 +46,7 @@ class DND_Class(db.Model):
     is_offical = db.Column(db.Boolean)
 
 class Character_Class(db.Model):
+    __tablename__ = "character_class"
     char_id = db.Column(db.Integer, db.ForeignKey(Character.char_id), primary_key=True, nullable=False)
     class_id = db.Column(db.Integer, db.ForeignKey(DND_Class.class_id), nullable=False)
     class_level = db.Column(db.Integer, nullable=False)
