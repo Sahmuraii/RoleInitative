f9fa56f0e7809d54e630cd11fd3f95e92f887182
Sahmuraii <vericwilliam@gmail.com>
2024-12-01 18:16:22 -0800

    seeding class features

--- src/utils/populate_database.py
+++ src/utils/populate_database.py
@@ -2,13 +2,14 @@ from src import db
 import requests
 
 from src.auth.models import User
-from src.character.models import DND_Class, DND_Race, DND_Condition
+from src.character.models import DND_Class, DND_Race, DND_Condition, DND_Class_Feature
 
 DND_BASE_URL = "https://www.dnd5eapi.co"
 API_BASE_URL = "https://www.dnd5eapi.co/api"
 API_CLASS_URL = "https://www.dnd5eapi.co/api/classes"
 API_RACE_URL = "https://www.dnd5eapi.co/api/races"
 API_CONDITION_URL = "https://www.dnd5eapi.co/api/conditions"
+API_CLASS_FEATURE_URL = "https://www.dnd5eapi.co/api/features"
 
 #Takes in a URL to an api, as well as any necessary headers
 #Returns the success of the API call, and the returned response (if any)
@@ -150,8 +151,46 @@ def fetch_and_populate_conditions():
         db.session.rollback()
         print(f"An error occurred: {e}")
 
+def fetch_and_populate_class_features():
+    success, response = fetch_api_info(API_CLASS_FEATURE_URL, headers={"Accept": "application/json"})
+    if not success: return
+
+    features = response.json().get("results", [])
+
+    for feature in features:
+        # Check if the condition already exists
+        existing_feature = DND_Class_Feature.query.filter_by(feature_name=feature["name"]).first()
+        if existing_feature: continue #If exist, continue to next one
+
+        # Fetch detailed information for each condition
+        success, api_details_response = fetch_api_info(f"{DND_BASE_URL}{feature['url']}", headers={"Accept": "application/json"})
+
+        # Check if the request was successful. Skip to the next condition
+        if not success: continue
+        
+        feature_details = api_details_response.json() # Extract the condition details
+
+        # Prepare the DND_Condition object to be added to the database
+        new_feature = DND_Class_Feature(
+            feature_name = feature_details["name"],
+            feature_description = "\n".join(feature_details["desc"]),
+            feature_prerequisite = feature_details["prerequisites"],
+            feature_required_level = feature_details["level"],
+            feature_base_class = DND_Class.query.filter_by(name=feature_details["class"]["name"]).first().class_id
+        )
+        
+        db.session.add(new_feature)
+    
+    try:
+        db.session.commit()
+        print("Features successfully populated!")
+    except Exception as e:
+        db.session.rollback()
+        print(f"An error occurred: {e}")
+        
 
 def repopulate_empty_tables():
     fetch_and_populate_classes()
     fetch_and_populate_races()
     fetch_and_populate_conditions()
+    fetch_and_populate_class_features()
