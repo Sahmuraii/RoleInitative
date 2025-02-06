c293a20b936778ec27320423592641a0e3054738
Yensid626 <43460104+Yensid626@users.noreply.github.com>
2025-02-06 00:09:58 -0800

    Initial Angular Character Sheet
    Updating the bare bones framework to start work on viewing Character Sheets in Angular.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { CreateBackgroundComponent } from './create-background/create-background.component'; 
 import { HomeComponent } from './home/home.component';
+import { CharacterSheetComponent } from './character-sheet/character-sheet.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'create/background', component: CreateBackgroundComponent },
+  { path: 'character-sheet', component: CharacterSheetComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' }, 
-  { path: '**', redirectTo: '/home' } 
+  { path: '**', redirectTo: '/home' }
 ];
---
+++ frontend/src/app/character-sheet/character-sheet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-sheet',
+  standalone: true,
+  imports: [],
+  templateUrl: './character-sheet.component.html',
+  styleUrl: './character-sheet.component.css'
+})
+export class CharacterSheetComponent {
+
+}
