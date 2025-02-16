3b27039f6673923bf0edfc042ff7050ba66d11d6
Sahmuraii <vericwilliam@gmail.com>
2025-02-15 19:11:59 -0800

    possible fix?

--- frontend/src/app/create-character/create-character.component.ts
+++ frontend/src/app/create-character/create-character.component.ts
@@ -14,17 +14,13 @@ import { DND_Race } from '../models/dnd_race.type';
   styleUrl: './create-character.component.css'
 })
 export class CreateCharacterComponent implements OnInit {
-  characterForm = this.fb.group({
-    
-  })
+  characterForm: FormGroup; // Changed Initalization of form group
+
   dndClassesSignal = signal<Array<DND_Class>>([])
   dndRaces = signal<Array<DND_Race>>([])
 
-
-
   totalLevel = signal(0);
   primaryClass = signal(null)
-
 
   hideBasicInfo = false;
   hideRace = true;
@@ -33,20 +29,43 @@ export class CreateCharacterComponent implements OnInit {
   hideAttributes = true;
   hideDetails = true;
   hideEquipment = true;
+
   createCharacterService = inject(CreateCharacterService)
 
-  constructor(private fb: FormBuilder) {}
+  // Cosntructor changed to initalized formGroup
+  constructor(private fb: FormBuilder) {
+    // Initialize the form with a FormArray for class levels
+    this.characterForm = this.fb.group({
+      classLevels: this.fb.array([]) // FormArray to store class levels
+    });
+  }
+
+  get classLevels(): FormArray {
+    return this.characterForm.get('classLevels') as FormArray;
+  }
+
+  initializeClassLevels(classes: DND_Class[]): void {
+    classes.forEach(() => {
+      this.classLevels.push(this.fb.control(0, [Validators.min(0), Validators.max(20)]));
+    });
+  }
+
+  getClassLevels(): { class_name: string, level: number }[] {
+    return this.dndClassesSignal().map((dndClass, index) => ({
+      class_name: dndClass.name,
+      level: this.classLevels.at(index).value ?? 0
+    }));
+  }
 
   ngOnInit(): void {
     this.createCharacterService.getClassData().subscribe((classes) => {
       this.dndClassesSignal.set(classes)
+      this.initializeClassLevels(classes); // Initialize the FormArray with class levels
     })
     this.createCharacterService.getRaceData().subscribe((races) => {
       this.dndRaces.set(races)
     })
-    
   }
-
   
   public showTab(tabId: string) {
     switch(tabId) {
